972fe4f790b618ea323188f42c6e97f5b0c5fc8a
praveen.mada <praveen.mada@dish.com>
2023-11-14 10:53:19 -0700

    findr infra automation

--- variables.tf
+++ variables.tf
@@ -65,3 +65,82 @@ variable "backend_dynamodb_table_name" {
   default     = null
   sensitive   = true
 }
+#findr variables
+variable "existing_subnet_route_table_id" {
+  description = "route table id"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "existing_vpc_id" {
+  description = "vpc id"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "existing_subnet_ids_1" {
+  description = "subnets 1"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "existing_subnet_ids_2" {
+  description = "subnets 2"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "existing_eks_iam_role_arn" {
+  description = "iam role for eks cluster"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "existing_node_group_iam_role_arn" {
+  description = "iam role for node groups"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_cluster_name_1" {
+  description = "unique cluster name 1"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_cluster_name_2" {
+  description = "unique cluster name 2"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_cluster_name_3" {
+  description = "unique cluster name 3"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_node_group_name_1" {
+  description = "unique node group name 1"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_node_group_name_2" {
+  description = "unique node group name 2"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_node_group_name_3" {
+  description = "unique node group name 3"
+  type        = string
+  default     = null
+  sensitive   = true
+}
+variable "eks_node_instance_types" {
+  description = "instance types"
+  type        = string
+  default     = null
+  sensitive   = true
+}

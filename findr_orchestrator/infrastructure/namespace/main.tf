cfa4f61918243a20b49107f836e1e3fcfe8bbed6
Praveen Mada <praveen.mada@dish.com>
2023-12-05 14:20:18 -0700

    namespace main.tf provider update

--- findr_orchestrator/infrastructure/namespace/main.tf
+++ findr_orchestrator/infrastructure/namespace/main.tf
@@ -1,21 +1,6 @@
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  config_path = "~/.kube/config"
 }
-
-
-# provider "kubernetes" {
-#   config_path = "~/.kube/config"
-# }
 
 resource "kubernetes_namespace" "orchestrator" {
   metadata {

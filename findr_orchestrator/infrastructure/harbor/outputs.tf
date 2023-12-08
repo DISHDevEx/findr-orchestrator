95e761866c83bb6c9147d85a591f3e567506c6ff
praveen.mada <praveen.mada@dish.com>
2023-12-08 15:08:32 -0700

    habor debugging

--- findr_orchestrator/infrastructure/harbor/outputs.tf
+++ findr_orchestrator/infrastructure/harbor/outputs.tf
@@ -4,33 +4,28 @@
  * The endpoint URL of the deployed Harbor instance
  */
 output "harbor_endpoint" {
-
   description = "The endpoint URL of Harbor instance"
 
-  value = helm_release.harbor.manifest.values["expose"]["tls"]["hosts"][0]
-
+  # Construct the endpoint URL assuming standard DNS naming conventions
+  value = "https://${var.harbor_namespace}.svc.cluster.local"
 }
 
 /** 
  * The namespace where Harbor was installed
 */
 output "harbor_namespace" {
-
   description = "Namespace where Harbor was installed"
 
   value = var.harbor_namespace
-
 }
 
 /**
  * The password for the main Harbor admin user 
  */
 output "harbor_admin_password" {
-
   description = "Password for the main admin user of Harbor"
 
   value = var.harbor_password
 
   sensitive = true
-
 }

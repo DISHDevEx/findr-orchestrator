cdba674f719eaae0268225a128be89f0fdc35a04
John Liu <zheng.liu@dish.com>
2023-12-20 14:33:00 -0700

    update outputs under oracle

--- findr_orchestrator/infrastructure/oracle/outputs.tf
+++ findr_orchestrator/infrastructure/oracle/outputs.tf
@@ -2,8 +2,9 @@
 Output for LoadBalancer IP
 This output provides instructions on how to retrieve the external IP address of the LoadBalancer.
 This external IP is used to access the oracle and API.
+"Use 'kubectl get svc -n oracle -o wide' to find the external IP address of oracle's LoadBalancer service."
 **/
 output "oracle_url" {
   description = "Instruction to find the external IP address of oracle's LoadBalancer"
-  value       = "Use 'kubectl get svc -n oracle -o wide' to find the external IP address of oracle's LoadBalancer service."
+  value       = kubernetes_service.oracle_service.load_balancer_ingress[0].hostname
 }

8bbb679c3052c65b5ed020347394f09b3a270943
praveen.mada <praveen.mada@dish.com>
2023-12-27 10:53:03 -0700

    working orchestrator deployment setup

---
+++ findr_orchestrator/infrastructure/orchestrator/outputs.tf
@@ -0,0 +1,13 @@
+
+# Outputs
+# -------
+output "pod_name" {
+  value       = kubernetes_pod.orchestrator_pod.metadata[0].name
+  description = "The name of the deployed Kubernetes pod."
+  sensitive = true
+}
+
+output "load_balancer_ip" {
+  description = "Instruction to find the external IP address of Pod's LoadBalancer"
+  value       = "Use 'kubectl get svc -n var.namespace -o wide' to find the external IP address of Pod's LoadBalancer service."
+}

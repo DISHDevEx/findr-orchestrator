ba772b0a424fcc116146665069266a072058d3dc
RABhatty <rizwanabidbhatty@gmail.com>
2023-12-27 17:27:46 -0700

    adding oracle-ui files

--- findr_orchestrator/infrastructure/oracle/main.tf
+++ findr_orchestrator/infrastructure/oracle/main.tf
@@ -2,41 +2,51 @@ provider "aws" {
   region = var.eks_cluster_region
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
 }
 
-resource "kubernetes_pod" "nginx" {
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--region", var.eks_cluster_region, "--cluster-name", var.cluster_name]
+  }
+}
+
+resource "kubernetes_pod" "oracle_pod" {
   metadata {
-    name      = "nginx"
+    name      = "oracle-pod"
     namespace = var.namespace
   }
 
   spec {
     container {
-      image = var.nginx_image
-      name  = "nginx"
+      image = var.container_image
+      name  = "oracle"
       port {
-        container_port = var.nginx_container_port
+        container_port = var.container_port
       }
     }
   }
 }
 
-resource "kubernetes_service" "nginx" {
+resource "kubernetes_service" "oracle_lb" {
   metadata {
-    name      = "nginx"
+    name      = "oracle-lb"
     namespace = var.namespace
   }
 
   spec {
     selector = {
-      app = "nginx"
+      app = "oracle"
     }
 
     port {
-      port        = var.nginx_service_port
-      target_port = var.nginx_container_port
+      port        = var.container_port
+      target_port = var.container_port
     }
 
     type = "LoadBalancer"

333111160021b8ba369ec732244c99ae184ece16
praveen.mada <praveen.mada@dish.com>
2023-12-28 14:43:19 -0700

    tag upgrade

--- findr_orchestrator/infrastructure/orchestrator/variables.tf
+++ findr_orchestrator/infrastructure/orchestrator/variables.tf
@@ -116,6 +116,6 @@ variable "container_image" {
    /**
    * Default container image
    */ 
-  default = "docker.io/pravnreddy429/findr_orchestrator:v8"
+  default = "docker.io/pravnreddy429/findr_orchestrator:v9"
 
 }

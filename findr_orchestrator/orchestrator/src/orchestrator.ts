9fba41247f689ef03156b08804d22190288ec0be
praveen.mada <praveen.mada@dish.com>
2023-12-29 09:32:51 -0700

    reverting back

--- findr_orchestrator/orchestrator/src/orchestrator.ts
+++ findr_orchestrator/orchestrator/src/orchestrator.ts
@@ -1,14 +1,13 @@
+// orchestrator.ts
+
 import { exec } from 'child_process';
-import { writeFile, access, chmod } from 'fs/promises';
-import { constants } from 'fs';
+import { writeFile } from 'fs/promises';
 import dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
 
 export class Orchestrator {
-  private terraformPath = path.join(__dirname, 'src', 'k8s-resources');
-
+  private terraformPath = './src/k8s-resources/';
   async deploy(params: any): Promise<string> {
     
     const tfVars = {
@@ -21,13 +20,7 @@ export class Orchestrator {
       container_image: params.container_image
     };
 
-    try {
-      await this.checkAndSetWriteAccess(this.terraformPath);
-      await this.createTfVarsFile(tfVars);
-    } catch (error) {
-      console.error(`Error in deployment process: ${error}`);
-      throw error;
-    }
+    await this.createTfVarsFile(tfVars);
 
     return new Promise((resolve, reject) => {
       exec('terraform init && TF_LOG=DEBUG terraform apply -auto-approve', { cwd: this.terraformPath }, (error, stdout, stderr) => {
@@ -44,26 +37,11 @@ export class Orchestrator {
   }
 
   private async createTfVarsFile(tfVars: any): Promise<void> {
-    const filePath = path.join(this.terraformPath, 'terraform.tfvars.json');
-    await writeFile(filePath, JSON.stringify(tfVars, null, 2));
-    await this.setWriteAccess(filePath);
-  }
-
-  private async checkAndSetWriteAccess(directory: string): Promise<void> {
     try {
-      await access(directory, constants.W_OK);
-    } catch {
-      console.log(`Setting write permission for the directory: ${directory}`);
-      await this.setWriteAccess(directory);
-    }
-  }
-
-  private async setWriteAccess(filePath: string): Promise<void> {
-    try {
-      // 0o666 sets read and write permissions for owner, group and others
-      await chmod(filePath, 0o666);
+      await writeFile(`${this.terraformPath}terraform.tfvars.json`, JSON.stringify(tfVars, null, 2));
     } catch (error) {
-      throw new Error(`Failed to set write access for ${filePath}: ${error}`);
+      console.error(`File write error: ${error}`);
+      throw error;
     }
   }
 }

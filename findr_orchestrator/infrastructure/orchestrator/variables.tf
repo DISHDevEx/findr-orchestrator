b6edaba60c6e596f1938fb34243944e29f1948f9
praveen.mada <praveen.mada@dish.com>
2023-12-29 10:13:43 -0700

    updated dockerfile

--- findr_orchestrator/infrastructure/orchestrator/variables.tf
+++ findr_orchestrator/infrastructure/orchestrator/variables.tf
@@ -116,6 +116,6 @@ variable "container_image" {
    /**
    * Default container image
    */ 
-  default = "docker.io/pravnreddy429/findr_orchestrator:v19"
+  default = "docker.io/pravnreddy429/findr_orchestrator:v20"
 
 }

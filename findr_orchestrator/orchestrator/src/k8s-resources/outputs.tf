a4fe4a7a44d517fa1e4ec56a4f12306eb1caa8a3
praveen.mada <praveen.mada@dish.com>
2024-01-02 14:04:47 -0700

    terraform resource cleanup updates

--- findr_orchestrator/orchestrator/src/k8s-resources/outputs.tf
+++ findr_orchestrator/orchestrator/src/k8s-resources/outputs.tf
@@ -2,7 +2,7 @@
 # Outputs
 # -------
 output "pod_name" {
-  value       = kubernetes_pod.findr_pod.metadata[0].name
+  value       = kubernetes_deployment.findr_pod.metadata[0].name
   description = "The name of the deployed Kubernetes pod."
   sensitive = true
 }

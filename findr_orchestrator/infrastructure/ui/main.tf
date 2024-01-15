cfb7383669242272d0de7cbe4a7529f2b4a7d829
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2024-01-15 16:14:20 -0700

    export IP to  be used as environment variables

--- findr_orchestrator/infrastructure/ui/main.tf
+++ findr_orchestrator/infrastructure/ui/main.tf
@@ -21,6 +21,29 @@ provider "kubernetes" {
   }
 }
 
+#Get NodePort and NodeIP of Oracle for API endpoint
+resource "null_resource" "export_oracle" {
+  triggers = {
+    # Trigger when the service or any relevant configuration changes
+    service_version = kubernetes_service.oracle-service.metadata[0].resource_version
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+    #get nodeport
+      NODEPORT=\$(kubectl get service oracle-service -o=jsonpath='{.spec.ports[0].nodePort}')
+      export ORACLE_NODE_PORT
+    #get node
+      POD_NAME=$(kubectl get pod -n oracle -l app=oracle -o jsonpath='{.items[0].metadata.name}')
+      NODE_NAME=$(kubectl get pod $POD_NAME -n oracle -o jsonpath='{.spec.nodeName}')
+      NODE_IP=$(kubectl get node $NODE_NAME -o jsonpath='{.status.addresses[?(@.type=="ExternalIP")].address}')
+      export ORACLE_NODE_IP
+    EOT
+  }
+}
+
+
+
 # Kubernetes Deployment
 resource "kubernetes_deployment" "ui_deployment" {
   metadata {
@@ -43,11 +66,19 @@ resource "kubernetes_deployment" "ui_deployment" {
           app = "ui"
         }
       }
-
+    
       spec {
         container {
           name  = "ui-container"
           image = "docker.io/pravnreddy429/findr_ui"  # Specify your Docker Hub image
+          env {
+            name  = "REACT_APP_ORACLE_NODE_PORT"
+            value = "${null_resource.export_oracle.ORACLE_NODE_PORT"
+          }
+          env {
+            name  = "REACT_APP_ORACLE_NODE_IP"
+            value = "${null_resource.export_oracle.ORACLE_NODE_IP}"
+          }
         }
       }
     }

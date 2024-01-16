70da3f0a8537ae85acdb643f0ce86f531a427611
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2024-01-16 11:59:16 -0700

    built oracle url with /oracle

--- findr_orchestrator/infrastructure/ui/get_oracle_node.sh
+++ findr_orchestrator/infrastructure/ui/get_oracle_node.sh
@@ -10,7 +10,7 @@ NODE_NAME=$(kubectl get pod $POD_NAME -n oracle -o jsonpath='{.spec.nodeName}')
 NODE_IP=$(kubectl get node $NODE_NAME -o jsonpath='{.status.addresses[?(@.type=="ExternalIP")].address}')
 
 #create oracle URL
-ORACLE_URL="http://${NODE_IP}:${NODE_PORT}"  
+ORACLE_URL="http://${NODE_IP}:${NODE_PORT}/oracle"  
 
 #create json to set as external data in terraform
 jq -n --arg oracle_url "$ORACLE_URL" '{"oracle_url":$oracle_url}'

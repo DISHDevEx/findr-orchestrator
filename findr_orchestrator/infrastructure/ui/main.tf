02529782c490d6a0285e7ceca9b51977f213b06d
lauren.mieczkowski <lauren.mieczkowski@dish.com>
2024-01-16 09:33:10 -0700

    added script to get oracle node information for ui env input

--- findr_orchestrator/infrastructure/ui/main.tf
+++ findr_orchestrator/infrastructure/ui/main.tf
@@ -22,30 +22,19 @@ provider "kubernetes" {
 }
 
 #Get NodePort and NodeIP of Oracle for API endpoint
-resource "null_resource" "export_oracle" {
-  triggers = {
-    # Trigger when the service or any relevant configuration changes
-    service_version = kubernetes_service.oracle-service.metadata[0].resource_version
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-    #get nodeport
-      NODEPORT=\$(kubectl get service oracle-service -o=jsonpath='{.spec.ports[0].nodePort}')
-      export ORACLE_NODE_PORT
-    #get node
-      POD_NAME=$(kubectl get pod -n oracle -l app=oracle -o jsonpath='{.items[0].metadata.name}')
-      NODE_NAME=$(kubectl get pod $POD_NAME -n oracle -o jsonpath='{.spec.nodeName}')
-      NODE_IP=$(kubectl get node $NODE_NAME -o jsonpath='{.status.addresses[?(@.type=="ExternalIP")].address}')
-      export ORACLE_NODE_IP
-    EOT
-  }
+data "external" "oracle_info" {
+  program = ["bash", "${path.module}/get_oracle_node.sh"]
 }
 
+locals {
+  oracle_url = data.external.oracle_info.result.oracle_url
+}
 
 
 # Kubernetes Deployment
 resource "kubernetes_deployment" "ui_deployment" {
+  depends_on = [data.external.oracle_info]
+
   metadata {
     name      = "ui"
     namespace = var.ui_namespace
@@ -72,12 +61,8 @@ resource "kubernetes_deployment" "ui_deployment" {
           name  = "ui-container"
           image = "docker.io/pravnreddy429/findr_ui"  # Specify your Docker Hub image
           env {
-            name  = "REACT_APP_ORACLE_NODE_PORT"
-            value = "${null_resource.export_oracle.ORACLE_NODE_PORT}"
-          }
-          env {
-            name  = "REACT_APP_ORACLE_NODE_IP"
-            value = "${null_resource.export_oracle.ORACLE_NODE_IP}"
+            name  = "REACT_APP_ORACLE_URL"
+            value = "${local.oracle_url}"
           }
         }
       }
--- findr_orchestrator/infrastructure/ui/outputs.tf
+++ findr_orchestrator/infrastructure/ui/outputs.tf
@@ -1,13 +1,3 @@
-/** 
-Output for LoadBalancer IP
-This output provides instructions on how to retrieve the external IP address of the LoadBalancer.
-This external IP is used to access the ui and API.
-"Use 'kubectl get svc -n ui -o wide' to find the external IP address of ui's LoadBalancer service."
-
-
-output "ui_url" {
-  description = "Instruction to find the external IP address of ui's LoadBalancer"
-  value       = kubernetes_service.ui_service.load_balancer_ingress[0].hostname
+output "oracle_url" {
+  value = local.oracle_url
 }
-
-**/
